e483f9c6d9be4cb8f4aa18b81f1f0834757bf3e3
AymaneBOUJELLABA <aymane.boujellaba@gmail.com>
2022-03-09 10:14:03 +0100

    initial commit

---
+++ src/main/java/fr/ubo/dosi/CSCIEVAE/CsciEvaeApplication.java
@@ -0,0 +1,13 @@
+package fr.ubo.dosi.CSCIEVAE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsciEvaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsciEvaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ubo/dosi/CSCIEVAE/CsciEvaeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ubo.dosi.CSCIEVAE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsciEvaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
