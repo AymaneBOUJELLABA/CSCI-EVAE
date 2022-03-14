7be6f29dc544cb9696e0c99d389a8d2583730cc1
CHAIB Nawar <nawar.chaib@outlook.com>
2022-03-14 15:29:53 +0100

    unit tests

--- src/main/java/fr/ubo/dosi/CSCIEVAE/controller/UniteEnseignementController.java
+++ src/main/java/fr/ubo/dosi/CSCIEVAE/controller/UniteEnseignementController.java
@@ -5,17 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
-
-import javax.websocket.server.PathParam;
-
 import fr.ubo.dosi.CSCIEVAE.entity.UniteEnseignement;
 import fr.ubo.dosi.CSCIEVAE.service.UniteEnseignementService;
 
 @RestController
+@RequestMapping("/ens")
 public class UniteEnseignementController {
 	@Autowired
 	private UniteEnseignementService uniteEnseignementService;
@@ -35,7 +33,6 @@ public class UniteEnseignementController {
 		return this.uniteEnseignementService.getByCodeUe(code);
 	}
 	
-	//unités enseignement de la promotion dosi 2013-2014
 	@GetMapping("UniteEnseignements/{code}")
 	public List<Map<String,String>> ListerUeByFormation(@PathVariable String code){
 		return this.uniteEnseignementService.ListByCodeFormation(code);
--- src/main/java/fr/ubo/dosi/CSCIEVAE/repository/UniteEnseignementRepository.java
+++ src/main/java/fr/ubo/dosi/CSCIEVAE/repository/UniteEnseignementRepository.java
@@ -1,9 +1,11 @@
 package fr.ubo.dosi.CSCIEVAE.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.ubo.dosi.CSCIEVAE.entity.UniteEnseignement;
 
+@Repository
 public interface UniteEnseignementRepository extends JpaRepository<UniteEnseignement,Long> {
 
 }
