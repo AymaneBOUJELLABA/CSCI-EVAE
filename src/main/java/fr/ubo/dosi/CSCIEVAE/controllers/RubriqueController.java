1b169e12174ec5d7f5f83305137346dfc80a45b1
AymaneBOUJELLABA <59460654+AymaneBOUJELLABA@users.noreply.github.com>
2022-03-15 10:20:42 +0100

    Update RubriqueController.java

--- src/main/java/fr/ubo/dosi/CSCIEVAE/controllers/RubriqueController.java
+++ src/main/java/fr/ubo/dosi/CSCIEVAE/controllers/RubriqueController.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,7 @@ import fr.ubo.dosi.CSCIEVAE.messages.RubriqueOrdreUpdateMessage;
 import fr.ubo.dosi.CSCIEVAE.services.RubriqueService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/rubriques")
 public class RubriqueController
 {
