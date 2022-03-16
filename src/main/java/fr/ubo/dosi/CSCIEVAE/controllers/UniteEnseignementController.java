0975f50c655541a62a1f2825ee7c9f8846237d0a
KHALIL EL AZOUZI <elazouzi.khalil@gmail.com>
2022-03-16 15:36:47 +0100

    update on files for merge done

--- src/main/java/fr/ubo/dosi/CSCIEVAE/controllers/UniteEnseignementController.java
+++ src/main/java/fr/ubo/dosi/CSCIEVAE/controllers/UniteEnseignementController.java
@@ -1,18 +1,15 @@
-package fr.ubo.dosi.CSCIEVAE.controller;
+package fr.ubo.dosi.CSCIEVAE.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Map;
 
 import fr.ubo.dosi.CSCIEVAE.entity.UniteEnseignement;
 import fr.ubo.dosi.CSCIEVAE.service.UniteEnseignementService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/ue")
 public class UniteEnseignementController {
