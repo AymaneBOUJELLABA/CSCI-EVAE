efb094b152986fcb393ec1289949182f589be6eb
KHALIL EL AZOUZI <elazouzi.khalil@gmail.com>
2022-03-17 09:10:59 +0100

    simple updates

--- src/main/java/fr/ubo/dosi/CSCIEVAE/CsciEvaeApplication.java
+++ src/main/java/fr/ubo/dosi/CSCIEVAE/CsciEvaeApplication.java
@@ -4,19 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("fr.ubo.dosi.CSCIEVAE.entity") 
-public class CsciEvaeApplication implements CommandLineRunner {
+public class CsciEvaeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsciEvaeApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args){
-
-	}
 }
