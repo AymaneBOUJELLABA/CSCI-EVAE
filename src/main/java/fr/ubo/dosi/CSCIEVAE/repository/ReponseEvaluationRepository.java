e409f5021497872379c5b9fd8f845b3ef0ae6ef5
AymaneBOUJELLABA <59460654+AymaneBOUJELLABA@users.noreply.github.com>
2022-03-23 15:19:05 +0100

    Create ReponseEvaluationRepository.java

---
+++ src/main/java/fr/ubo/dosi/CSCIEVAE/repository/ReponseEvaluationRepository.java
@@ -0,0 +1,8 @@
+package fr.ubo.dosi.CSCIEVAE.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReponseEvaluationRepository extends JpaRepository<Object, Long>
+{
+	
+}
